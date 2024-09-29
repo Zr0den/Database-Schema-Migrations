411e3a7dfac4a352e80641770091f366a47d4547
Zr0den <axellund7@gmail.com>
2024-09-29 12:36:40 +0700

    Before the first migration - setup EF and the product table

---
+++ EF Migrations/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Migrations.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ EF Migrations/Model.cs
@@ -0,0 +1,50 @@
+﻿using EF_Migrations.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace EF_Migrations
+{
+    public class Model : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public string DbPath { get; }
+
+        public Model()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "Model.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer($"Data Source={DbPath}");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var decimalProps = modelBuilder.Model
+            .GetEntityTypes()
+            .SelectMany(t => t.GetProperties())
+            .Where(p => (System.Nullable.GetUnderlyingType(p.ClrType) ?? p.ClrType) == typeof(decimal));
+
+            foreach (var property in decimalProps)
+            {
+                property.SetPrecision(18);
+                property.SetScale(2);
+            }
+
+            modelBuilder.Entity<Product>(p =>
+            {
+                p.HasKey(e => e.Id);
+                p.Property(e => e.Id).UseIdentityColumn();
+            });
+        }
+    }
+}
