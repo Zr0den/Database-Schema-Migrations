1245e44525ef7c8c89cc8291d7c2488da882c8ee
Zr0den <axellund7@gmail.com>
2024-09-29 12:58:48 +0700

    Added categories and updated Product and created new migration

--- EF Migrations/Entities/Product.cs
+++ EF Migrations/Entities/Product.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,9 @@ namespace EF_Migrations.Entities
         public int Id { get; set; } 
         public string Name { get; set; }
         public decimal Price { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
--- EF Migrations/Migrations/20240929055802_AddCategories.Designer.cs
+++ EF Migrations/Migrations/20240929055802_AddCategories.Designer.cs
@@ -3,6 +3,7 @@ using EF_Migrations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EF_Migrations.Migrations
 {
     [DbContext(typeof(Model))]
-    partial class ModelModelSnapshot : ModelSnapshot
+    [Migration("20240929055802_AddCategories")]
+    partial class AddCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
